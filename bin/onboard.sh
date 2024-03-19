7dc5b28aafc8eb0694ed30972579159d019d9596
Andriy Novykov <andriy@safe.ai>
2024-03-18 22:36:05 -0700

    Added basic error handling.

--- bin/onboard.sh
+++ bin/onboard.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Variables (Replace these with actual values or pass them as arguments)
-lab_name='' # john_smith_lab
-researcher_name='' # john_smith
-full_name='' # John Smith
-user_password=''
-parent_account='' 
-ssh_key=''
+lab_name='' # john_smith
+cluster_username='' # john_smith
+full_name='Steven Basart 3' # John Smith
+user_password='password'
+parent_account='root'
+ssh_key='ssh-key'
+
+# Function to exit script upon error
+function error_exit {
+    echo "$1" 1>&2
+    exit 1
+}
 
 # Check if lab exists on the cluster
 if cluster group list | grep -w "$lab_name"; then
@@ -15,21 +21,21 @@ else
     # Lab Onboarding
     echo "Onboarding new lab: $lab_name"
     # Add the lab group
-    cluster group create "$lab_name"
+    cluster group create "$lab_name" || error_exit "Failed to create lab group: $lab_name"
     # Add slurm lab account
-    sudo sacctmgr add account --immediate "$lab_name" Parent="$parent_account" Description="Professor $lab_name Lab" Organization=Prof_"$lab_name"
+    sudo sacctmgr add account --immediate "$lab_name" Parent="$parent_account" Description="$lab_name Lab" Organization=Prof_"$lab_name" || error_exit "Failed to add Slurm lab account for: $lab_name"
 fi
 
 # Researcher Onboarding
-echo "Onboarding new researcher: $researcher_name"
+echo "Onboarding new researcher: $cluster_username"
 # Retrieve the lab group id
 group_id=$(cluster group list | grep -wA1 "$lab_name" | grep gidNumber | awk '{print $2}')
 # Create cluster user
-cluster user add "$researcher_name" --gid "$group_id" --password "$user_password" --name "$full_name"
+cluster user add "$cluster_username" --gid "$group_id" --password "$user_password" --name "$full_name" || error_exit "Failed to add cluster user: $cluster_username"
 # Create Slurm user
-sudo sacctmgr create user --immediate "$researcher_name" DefaultAccount="$lab_name"
+sudo sacctmgr create user --immediate "$cluster_username" DefaultAccount="$lab_name" || error_exit "Failed to create Slurm user for: $cluster_username"
 # Add SSH key 
-echo "$ssh_key" | sudo tee -a /data/$researcher_name/.ssh/authorized_keys
+echo "$ssh_key" | sudo tee -a /data/$cluster_username/.ssh/authorized_keys
 # Set Filesystem Quotas
-sudo weka fs quota set "/data/$researcher_name" --hard 1TB --grace 1d
-echo "Onboarding process completed for $researcher_name."
+sudo weka fs quota set "/data/$cluster_username" --hard 1TB --grace 1d || error_exit "Failed to set filesystem quotas for: $cluster_username"
+echo "Onboarding process completed for $cluster_username."
