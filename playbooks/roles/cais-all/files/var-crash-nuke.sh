c11d00821623634818efa47d507d8e7de0b4fef8
jasonlim-cerberus <jasonlim@safe.ai>
2026-03-10 15:50:15 -0700

    removes indentation

--- playbooks/roles/cais-all/files/var-crash-nuke.sh
+++ playbooks/roles/cais-all/files/var-crash-nuke.sh
@@ -1,11 +1,10 @@
-     #!/usr/bin/env bash
-     set -euo pipefail
+#!/usr/bin/env bash
+set -euo pipefail
 
-     if [[ $EUID -ne 0 ]]; then
-         exec sudo "$0" "$@"
-     fi
+if [[ $EUID -ne 0 ]]; then
+    exec sudo "$0" "$@"
+fi
 
-     if [ -d /var/crash ]; then
-         rm -rf /var/crash/*
-     fi
-
+if [ -d /var/crash ]; then
+    rm -rf /var/crash/*
+fi
